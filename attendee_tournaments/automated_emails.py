10e977acb04fa4c7bec08fa0849bab8cf855cfd2
Rob Ruana <rob@robruana.com>
2017-03-12 20:31:07 -0700

    Fixes #2383: Adds static ident arg to each AutomatedEmail instance

--- attendee_tournaments/automated_emails.py
+++ attendee_tournaments/automated_emails.py
@@ -3,10 +3,13 @@ from attendee_tournaments import *
 AutomatedEmail.queries[AttendeeTournament] = lambda session: session.query(AttendeeTournament).all()
 
 AutomatedEmail(AttendeeTournament, 'Your tournament application has been received', 'tournament_app_received.txt',
-               lambda app: app.status == c.NEW)
+               lambda app: app.status == c.NEW,
+               ident='tournament_app_received')
 
 AutomatedEmail(AttendeeTournament, 'Your tournament application has been approved', 'tournament_app_accepted.txt',
-               lambda app: app.status == c.ACCEPTED)
+               lambda app: app.status == c.ACCEPTED,
+               ident='tournament_app_accepted')
 
 AutomatedEmail(AttendeeTournament, 'Your tournament application has been declined', 'tournament_app_declined.txt',
-               lambda app: app.status == c.DECLINED)
+               lambda app: app.status == c.DECLINED,
+               ident='tournament_app_declined')
