605900e1f26c0a60f429a6e6cc1792d66ad65fd8
Rob Ruana <rob@robruana.com>
2017-11-11 14:56:08 -0500

    Fixes magfest/ubersystem#2611: Adds console dept as sender for attendee tournaments

--- attendee_tournaments/automated_emails.py
+++ attendee_tournaments/automated_emails.py
@@ -4,12 +4,15 @@ AutomatedEmail.queries[AttendeeTournament] = lambda session: session.query(Atten
 
 AutomatedEmail(AttendeeTournament, 'Your tournament application has been received', 'tournament_app_received.txt',
                lambda app: app.status == c.NEW,
+               sender=c.ART_EMAIL,
                ident='tournament_app_received')
 
 AutomatedEmail(AttendeeTournament, 'Your tournament application has been approved', 'tournament_app_accepted.txt',
                lambda app: app.status == c.ACCEPTED,
+               sender=c.ART_EMAIL,
                ident='tournament_app_accepted')
 
 AutomatedEmail(AttendeeTournament, 'Your tournament application has been declined', 'tournament_app_declined.txt',
                lambda app: app.status == c.DECLINED,
+               sender=c.ART_EMAIL,
                ident='tournament_app_declined')
